fc2792da54b30b024aea9253514a6265272e5c6a
yeremiamarcellius <yeremiamarcellius@gmail.com>
2020-12-31 00:21:01 +0700

    edit admin kurang validate

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -25,37 +25,39 @@
         <h1 class="text-white font-bold text-center text-3xl sm:text-4xl md:text-5xl">Dashboard</h1>
         <hr class="bg-white w-4/5 lg:w-1/2 my-8">
         <!-- Container Leader -->
-        <div class="flex flex-col mb-16 rounded-xl w-full w-full md:w-3/5 lg:w-1/2 bg-dust px-8 lg:px-16 2xl:px-32 py-12">
+        <form method ="POST" action="{{route('admin-update', $user->id)}}" class="flex flex-col mb-16 rounded-xl w-full w-full md:w-3/5 lg:w-1/2 bg-dust px-8 lg:px-16 2xl:px-32 py-12">
+            @csrf
+            @method('patch')            
             <h1 class="text-white text-center font-semibold text-3xl">Edit Data - Leader</h1>
             <hr class="w-full h-4 mt-6">
             <div class="flex flex-col mt-8">
                 <div class="w-full">
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">Full Name</label>
-                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" value="{{$user->fullname}}"></input>
+                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" name="full_name" value="{{$user->fullname}}"></input>
                 </div>
                 <div class="w-full">
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">Email</label>
-                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" value="{{$user->email_leader}}"></input>
+                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" name="email_leader" value="{{$user->email_leader}}"></input>
                 </div>
                 <div class="w-full">
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">Whatsapp Number</label>
-                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" value="{{$user->wa_num}}"></input>
+                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" name="wa_num" value="{{$user->wa_num}}"></input>
                 </div>
                 <div class="w-full">
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">Line ID</label>
-                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" value="{{$user->line_id}}"></input>
+                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" name="line_id" value="{{$user->line_id}}"></input>
                 </div>
                 <div class="w-full">
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">GitHub or Gitlub ID</label>
-                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" value="{{$user->github}}"></input>
+                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" name="github" value="{{$user->github}}"></input>
                 </div>
-                <div class="w-full">
+                    <div class="w-full">                        
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">Birth Place</label>
-                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" value="{{$user->birth_place}}"></input>
+                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" name="birth_place" value="{{$user->birth_place}}"></input>
                 </div>
                 <div class="w-full">
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">Birth Date</label>
-                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" value="{{$user->birth_day}}"></input>
+                    <input class="w-full text-white text-base font-light ml-2 rounded-lg border-white bg-transparent border-2 h-8" name="birth_day" value="{{$user->birth_day}}"></input>
                 </div>
                 <div class="w-full flex flex-col">
                     <label class="ml-2 mt-2 font-dust text-sm font-semibold">Upload CV</label>
@@ -74,7 +76,7 @@
                 </div>
                 <button type="submit" class="w-40 sm:w-60 bg-purples transform hover:scale-110 motion-reduce:transform-none focus:outline-none mt-6 rounded-lg text-white text-xl py-1 text-semibold">submit</button>
             </div>
-        </div>
+        </form>
         <!-- Container Member -->
         @foreach ($members as $member)
             @if ($member->group_id == $user->id)

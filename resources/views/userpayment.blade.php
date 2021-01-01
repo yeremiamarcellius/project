493c0a8ca232f77456ab677e029e50e4faedb163
ConvexHull89 <riosheren89@gmail.com>
2021-01-01 22:10:33 +0700

    route home selesai

--- resources/views/userpayment.blade.php
+++ resources/views/userpayment.blade.php
@@ -13,10 +13,10 @@
     <nav class="flex z-50 flex-row bg-gradient-to-b fixed top-0 from-purple-600 bg-opacity-50 justify-between w-full">
         <img src="/images/ehackathon_logo_1.png" alt="" class="transform hover:scale-110 motion-reduce:transform-none ml-0 sm:ml-4 md:ml-8" height="100px" width="100px"/>
         <div class="bg-ocean flex flex-row justify-around p-2 rounded-bl-3xl w-60 xl:w-80 h-1/2">
-            <a class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="../pages/user-home.html"><img height="50px" width="50px" src="/images/svgs/nav-home.svg" alt="nav.svg"></a>
+            <a class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="{{route('home')}}"><img height="50px" width="50px" src="/images/svgs/nav-home.svg" alt="nav.svg"></a>
             <a class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="#"><img height="50px" width="50px" src="/images/svgs/nav-payment.svg" alt="nav.svg"></a>
-            <a class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="../pages/user-timeline.html"><img height="50px" width="50px" src="/images/svgs/nav-timeline.svg" alt="nav.svg"></a>
-            <form><button type="submit" class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="../index.html"><img height="50px" width="50px" src="/images/svgs/nav-admin-logout.svg" alt="nav.svg"></button></form>
+            <a class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="{{route('hometimeline')}}"><img height="50px" width="50px" src="/images/svgs/nav-timeline.svg" alt="nav.svg"></a>
+            <form action="{{route('logout')}}" method ="POST"> @csrf<button type="submit" class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href=""><img height="50px" width="50px" src="/images/svgs/nav-admin-logout.svg" alt="nav.svg"></button></form>
         </div>
     </nav>
     <section class="min-h-screen bg-blacks py-32 px-8 sm:px-24">

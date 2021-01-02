1a0e2c1eb9b98ca00c23898ffbd9eceb2b070abd
ConvexHull89 <riosheren89@gmail.com>
2021-01-02 16:13:53 +0700

    admin delete selesai

--- resources/views/adminpayment.blade.php
+++ resources/views/adminpayment.blade.php
@@ -11,12 +11,12 @@
 <body class="m-0 p-0">
 <nav class="flex flex-row bg-gradient-to-b fixed top-0 from-purple-600 bg-opacity-50 justify-between w-full">
     <img src="/images/ehackathon_logo_1.png" alt="" class="transform hover:scale-110 motion-reduce:transform-none ml-0 sm:ml-4 md:ml-8" height="100px" width="100px"/>
-    <div class="bg-ocean flex flex-row justify-between rounded-bl-3xl p-2 w-60 xl:w-80 h-1/2">
-        <a class="mx-auto transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="{{route('admin')}}"><img height="50px" width="50px" src="/images/svgs/nav-admin-pay.svg" alt="nav.svg"></a>
-        <a class="mx-auto transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="{{route('admin-payment')}}"><img height="50px" width="50px" src="/images/svgs/nav-admin-part.svg" alt="nav.svg"></a>
+    <div class="bg-ocean flex flex-row justify-around rounded-bl-3xl p-2 w-60 xl:w-80 h-1/2">
+        <a class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="{{route('admin')}}"><img height="50px" width="50px" src="/images/svgs/nav-admin-pay.svg" alt="nav.svg"></a>
+        <a class="transform hover:scale-110 motion-reduce:transform-none flex justify-center items-center" href="{{route('admin-payment')}}"><img height="50px" width="50px" src="/images/svgs/nav-admin-part.svg" alt="nav.svg"></a>
         <form action="{{route('logout')}}" method="POST">
             @csrf
-            <button type="submit"><img height="50px" width="50px" src="/images/svgs/nav-admin-logout.svg" alt="nav.svg"></button>
+            <button type="submit"><img class="mt-2" height="50px" width="50px" src="/images/svgs/nav-admin-logout.svg" alt="nav.svg"></button>
         </form>
     </div>
 </nav>
@@ -37,7 +37,7 @@
                         <div class="flex w-full justify-around">
                             <button class="transform hover:scale-110 motion-reduce:transform-none"><a href="{{route('admin-view', $user->id)}}"><img class="w-12 sm:w-14 md:w-16 lg:w-18 xl:w-20" src="/images/svgs/view-dashboardA2.svg" alt="edit.svg"></a></button>
                             <button class="transform hover:scale-110 motion-reduce:transform-none"><a href="{{route('admin-edit', $user->id)}}"><img class="w-12 sm:w-14 md:w-16 lg:w-18 xl:w-20" src="/images/svgs/edit-dashboardA.svg" alt="edit.svg"></a></button>
-                            <form method="POST" action="{{route('admin-delete', $user->id)}}" class = "ml-3">
+                            <form  action="{{route('admin-delete', $user->id)}}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <a class="focus:outline-none delete-pop-up-btn transform hover:scale-110 motion-reduce:transform-none buttonZ"><img class="w-12 sm:w-14 md:w-16 lg:w-18 xl:w-20" src="/images/svgs/delete-dashboardA.svg" alt="edit.svg"></a>
@@ -51,8 +51,8 @@
                                         <p class="text-center font-semibold text-white text-sm">Delete Participant?</p>
                                     </div> 
                                     <div class="flex justify-center px-4 pt-4 space-x-2">
-                                        <button class="focus:outline-none cancel-btn transform hover:scale-110 motion-reduce:transform-none rounded-xl font-semibold bg-ocean text-white text-sm p-2">Cancel</button>
-                                        <button type="submit" class="focus:outline-none delete-btn transform hover:scale-110 motion-reduce:transform-none rounded-xl font-semibold bg-purples text-white text-sm p-2">Delete</button>
+                                         <a class="focus:outline-none cancel-btn transform hover:scale-110 motion-reduce:transform-none rounded-xl font-semibold bg-ocean text-white text-sm p-2">Cancel</a> 
+                                         <button type="submit" class="focus:outline-none  transform hover:scale-110 motion-reduce:transform-none rounded-xl font-semibold bg-purples text-white text-sm p-2">Delete</button>
                                     </div>
                                 </div>
                             </form>
